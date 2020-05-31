ac3417c2f0f55aae216bd3c6b9ad68272ecbe920
Piotr Palka <piotr.palka123@gmail.com>
2020-05-31 10:02:29 +0700

    http port update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http'); // 1 - Import Node.js core module
 
-var listeningPort = 8080;
+var listeningPort = 80;
 
 var server = http.createServer(function (req, res) {   // 2 - creating server
 
