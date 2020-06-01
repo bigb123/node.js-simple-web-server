4db47318741d21d4b47772be63827f657b288b47
Piotr Palka <piotr.palka123@gmail.com>
2020-06-01 23:09:20 +0700

    Added a way to kill the server after one response

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-var http = require('http'); // 1 - Import Node.js core module
+var http = require('http'); 
 
 var listeningPort = 80;
 
-var server = http.createServer(function (req, res) {   // 2 - creating server
+var server = http.createServer(function (req, res) {
 
-    res.writeHead(200, { 'Content-Type': 'text/html' }); 
-    // set response content    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body><p>Nodejs is hosting this page</p></body></html>');
     res.end();
-
+    // process.exit();  // server will provide one answer and exit the process
 });
 
-server.listen(listeningPort); //3 - listen for any incoming requests
+server.listen(listeningPort); 
 
 console.log('Node.js web server at port ' + listeningPort + ' is running..')
