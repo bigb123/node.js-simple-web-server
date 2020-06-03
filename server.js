d6b77000b833ad82ff07eee1f2678e7407f9013a
Piotr Palka <piotr.palka123@gmail.com>
2020-06-03 19:06:08 +0700

    Env var name fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body>');
     res.write('<p>Nodejs is hosting this page</p><br>');
-    res.write('<p>Message from Lambda: ' + process.env.JSON-MSG + '</p>');
+    res.write('<p>Message from Lambda: ' + process.env.JSON_MSG + '</p>');
     res.write('</body></html>');
     res.end();
     // process.exit();  // server will provide one answer and exit the process
@@ -16,4 +16,4 @@ var server = http.createServer(function (req, res) {
 server.listen(listeningPort); 
 
 console.log('Node.js web server at port ' + listeningPort + ' is running.');
-console.log('Env var passed by 3rd parties (eg: Lambda): ' + process.env.JSON-MSG);
+console.log('Env var passed by 3rd parties (eg: Lambda): ' + process.env.JSON_MSG);
