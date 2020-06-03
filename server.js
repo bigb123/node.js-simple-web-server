ebb57b3f3c9c76c64574b27d0146552612fcdbf9
Piotr Palka <piotr.palka123@gmail.com>
2020-06-03 18:53:14 +0700

    Reading env var value

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ var listeningPort = 80;
 var server = http.createServer(function (req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><p>Nodejs is hosting this page</p></body></html>');
+    res.write('<html><body>');
+    res.write('<p>Nodejs is hosting this page</p><br>');
+    res.write('<p>Message from Lambda: ' + process.env.JSON-MSG + '</p>');
+    res.write('</body></html>');
     res.end();
     // process.exit();  // server will provide one answer and exit the process
 });
 
 server.listen(listeningPort); 
 
-console.log('Node.js web server at port ' + listeningPort + ' is running..')
+console.log('Node.js web server at port ' + listeningPort + ' is running.');
+console.log('Env var passed by 3rd parties (eg: Lambda): ' + process.env.JSON-MSG);
